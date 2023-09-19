2e8fc840f8946073b0f67bb4cea2575019f98039
Lorenzo Cirillo <blazkblazk@gmail.com>
2023-09-19 18:50:50 +0200

    cmd_vel publisher node

---
+++ ros_ws/build/my_package/CMakeFiles/cmd_vel_pub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/lattinone/Desktop/Lorenzo/rp/rp_project/ros_ws/devel/lib/my_package/cmd_vel_pub"
+  "/home/lattinone/Desktop/Lorenzo/rp/rp_project/ros_ws/devel/lib/my_package/cmd_vel_pub.pdb"
+  "CMakeFiles/cmd_vel_pub.dir/bin/cmd_vel_pub_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmd_vel_pub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ros_ws/src/my_package/bin/cmd_vel_pub_node.cpp
@@ -0,0 +1,28 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+
+int main(int argc, char** argv)
+{
+    // Initialize the ROS node
+    ros::init(argc, argv, "cmd_vel_publisher");
+    ros::NodeHandle nh;
+
+    // Create a publisher for the cmd_vel topic
+    ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/robot0/cmd_vel", 10);
+
+    // Create a Twist message with desired linear and angular velocities
+    geometry_msgs::Twist cmd_vel_msg;
+    cmd_vel_msg.linear.x = 0.2;     // Linear velocity (m/s)
+    cmd_vel_msg.angular.z = 0.5;    // Angular velocity (rad/s)
+
+    ros::Rate loop_rate(10);  // Publish at a rate of 10 Hz
+
+    while (ros::ok())
+    {
+        cmd_vel_pub.publish(cmd_vel_msg);  // Publish the Twist message
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
